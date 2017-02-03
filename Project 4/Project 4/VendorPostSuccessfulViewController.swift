4df15a9411ca10f3edadbe3db456f7ae7071a97e
mcdanton <danhefter@gmail.com>
2017-02-03 15:32:40 -0500

    saving

--- Project 4/Project 4/VendorPostSuccessfulViewController.swift
+++ Project 4/Project 4/VendorPostSuccessfulViewController.swift
@@ -12,7 +12,6 @@ class VendorPostSuccessfulViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      FirebaseModel.sharedInstance.retrieveLocation()
       dismissViewController()
    }
 
