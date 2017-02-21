b82fa8fc8098099e6a04748d89fd5700f7c4625b
mcdanton <danhefter@gmail.com>
2017-02-21 12:43:55 -0500

    Tweaked button extension class for nicer shadow

--- Project 4/Project 4/ButtonExtensionClass.swift
+++ Project 4/Project 4/ButtonExtensionClass.swift
@@ -15,14 +15,13 @@ import UIKit
 
    override func draw(_ rect: CGRect) {
       super.draw(rect)
-      self.layer.shadowColor = UIColor.black.cgColor
-      self.layer.shadowOffset = CGSize(width: 5, height: 5)
-      self.layer.shadowOpacity = 1
-      self.layer.shadowRadius = 5
+      self.layer.shadowColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.5).cgColor
+      self.layer.shadowOffset = CGSize(width: 4, height: 4)
+      self.layer.shadowOpacity = 4
+      self.layer.shadowRadius = 4
       self.layer.masksToBounds = false
-      self.layer.cornerRadius = 4.0
-      self.layer.cornerRadius = 5
-      self.backgroundColor = UIColor(red: 235/255, green: 211/255, blue: 74/255, alpha: 1.0)
+      self.layer.cornerRadius = 10
+      self.layer.backgroundColor = UIColor(red: 239/255, green: 211/255, blue: 20/255, alpha: 1.0).cgColor
 
 
    }
