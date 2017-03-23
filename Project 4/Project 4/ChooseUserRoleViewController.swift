11dff930e580b6d3a70e7b1b9351a6c15c7db15c
mcdanton <danhefter@gmail.com>
2017-03-23 10:51:50 -0400

    Hid password entry

--- Project 4/Project 4/ChooseUserRoleViewController.swift
+++ Project 4/Project 4/ChooseUserRoleViewController.swift
@@ -7,20 +7,22 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ChooseUserRoleViewController: UIViewController {
    
    
    
-   
+   // MARK: View Loading
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
    
    
+   // MARK: Prepare For Segue
    // Passes along role of user (Vendor or Consumer) to sign in page
    
    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
